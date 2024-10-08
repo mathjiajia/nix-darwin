ebcc124a496b787dcfa197ab10b7c31248c23018
Jia Jia <mathjiajia@outlook.com>
2024-10-09 00:31:53 +0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+          # List packages installed in system profile. To search by name, run:
+          # # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            bat
+            # cmake
+            fish
+            cargo
+            # node
+            btop
+            fastfetch
+            fd
+            fzf
+            ripgrep
+            starship
+            tree
+            tree-sitter
+            yazi
+            zoxide
+            # swift-format
+            difftastic
+            delta
+            onefetch
+            lazygit
+            glow
+            poppler
+            python3
+            unar
+            hugo
+            pandoc
+            nixfmt-rfc-style
+            neovide
+            neovim
+            emacs
+            vscode
+            # zotero
+            zed
+            # zoom-us
+          ];
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          # programs.zsh.enable = true;  # default shell on catalina
+          programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Jias-MacBook-Pro
+      darwinConfigurations."Jias-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Jias-MacBook-Pro".pkgs;
+    };
+}
