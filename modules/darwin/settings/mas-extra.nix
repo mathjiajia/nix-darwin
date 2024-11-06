9a4399b359372dd97361a2c376573f1722ac3c2f
Jia Jia <mathjiajia@outlook.com>
2024-11-06 22:52:07 +0800

    nothing

--- modules/darwin/settings/mas-extra.nix
+++ modules/darwin/settings/mas-extra.nix
@@ -1,16 +1,16 @@
 {
   homebrew.masApps = {
-    "Codye" = 1516894961;
-    "DevHub" = 6476452351;
+    Codye = 1516894961;
+    DevHub = 6476452351;
     "Enchanted LLM" = 6474268307;
     "Friendly Streaming" = 553245401;
-    "HorizonChase2" = 1534920947;
-    "Noto" = 1459055246;
+    HorizonChase2 = 1534920947;
+    Noto = 1459055246;
     "Plain Text Editor" = 1572202501;
     "Pro Snooker & Pool 2024" = 1586517737;
-    "ScanScan" = 1249901692;
-    "Solitaire" = 1534193824;
-    "Xcode" = 497799835;
+    ScanScan = 1249901692;
+    Solitaire = 1534193824;
+    Xcode = 497799835;
     "西窗烛" = 912139104;
   };
 }
